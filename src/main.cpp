01a11557332ddc914973d26f78ea62513fc6f2a8
ana <anocas.rp@gmail.com>
2023-11-21 11:13:06 +0000

    removed PWM_instances array and put PWM_instance in Servo_t

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,6 @@
 #include "RP2040_PWM.h"
 #define _PWM_LOGLEVEL_ 3 
 
-
-RP2040_PWM* PWM_instances[4];
-
-float frequency=50;
-
 #define Servo1pin 0
 #define Servo2pin 1
 #define Servo3pin 2 
@@ -31,8 +26,10 @@ float frequency=50;
 struct Servo_t {
   int pin;
   float dc_min, dc_max;
+  RP2040_PWM* PWM_instance;
 };
 
+float frequency=50; //50Hz
 
 Servo_t Servo1 = {Servo1pin, 2, 13};
 Servo_t Servo2 = {Servo2pin, 2, 13};
@@ -41,10 +38,11 @@ Servo_t Servo4 = {Servo4pin, 3, 14};
 
 void initServos(){
 
-  PWM_instances[0] = new RP2040_PWM(Servo1.pin, frequency, Servo1.dc_min);
-  PWM_instances[1] = new RP2040_PWM(Servo2.pin, frequency, Servo2.dc_min);
-  PWM_instances[2] = new RP2040_PWM(Servo3.pin, frequency, Servo3.dc_min);
-  PWM_instances[3] = new RP2040_PWM(Servo4.pin, frequency, Servo4.dc_min);
+  Servo1.PWM_instance = new RP2040_PWM(Servo1.pin, frequency, Servo1.dc_min);
+  Servo2.PWM_instance = new RP2040_PWM(Servo2.pin, frequency, Servo2.dc_min);
+  Servo3.PWM_instance = new RP2040_PWM(Servo3.pin, frequency, Servo3.dc_min);
+  Servo4.PWM_instance = new RP2040_PWM(Servo4.pin, frequency, Servo4.dc_min);
+
 }
 
 void setServo(Servo_t Servo, int angle){  //angle is between 0 and 180
@@ -52,7 +50,7 @@ void setServo(Servo_t Servo, int angle){  //angle is between 0 and 180
   float dutyCycle = Servo.dc_min + (Servo.dc_max - Servo.dc_min) * (angle/180.0); //calculates dutyCycle from angle
   Serial.print("dutyCycle: ");
   Serial.println(dutyCycle);
-  PWM_instances[Servo.pin]->setPWM(Servo.pin, frequency, dutyCycle); //sets dutyCycle in servo
+  Servo.PWM_instance->setPWM(Servo.pin, frequency, dutyCycle); //sets dutyCycle in servo
 }
 
 void setup()
