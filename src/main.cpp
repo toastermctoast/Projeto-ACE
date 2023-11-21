5a6f4c7e34bba1f48dbc00eb9c652dc7af99014f
ana <anocas.rp@gmail.com>
2023-11-21 11:57:25 +0000

    small things

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,4 @@
-/****************************************************************************************************************************
-  basic_pwm.ino
-  For RP2040 boards
-  Written by Dr. Benjamin Bird
-
-  A basic example to get you up and running.
-
-  Library by Khoi Hoang https://github.com/khoih-prog/RP2040_PWM
-  Licensed under MIT license
-
-  The RP2040 PWM block has 8 identical slices. Each slice can drive two PWM output signals, or measure the frequency
-  or duty cycle of an input signal. This gives a total of up to 16 controllable PWM outputs. All 30 GPIO pins can be driven
-  by the PWM block
-*****************************************************************************************************************************/
-
-#include <Arduino.h>    
-#include <vector>
+#include <Arduino.h> 
 #include "RP2040_PWM.h"
 #define _PWM_LOGLEVEL_ 3 
 
@@ -31,10 +15,10 @@ struct Servo_t {
 
 float frequency=50; //50Hz
 
-Servo_t Servo1 = {Servo1pin, 2, 13};
-Servo_t Servo2 = {Servo2pin, 2, 13};
-Servo_t Servo3 = {Servo3pin, 2, 13};
-Servo_t Servo4 = {Servo4pin, 3, 14};
+Servo_t Servo1 = {Servo1pin, 2, 12};
+Servo_t Servo2 = {Servo2pin, 2.7, 12.8};
+Servo_t Servo3 = {Servo3pin, 2, 12};
+Servo_t Servo4 = {Servo4pin, 4, 14};
 
 void initServos(){
 
@@ -47,9 +31,9 @@ void initServos(){
 
 void setServo(Servo_t Servo, int angle){  //angle is between 0 and 180
   
-  float dutyCycle = Servo.dc_min + (Servo.dc_max - Servo.dc_min) * (angle/180.0); //calculates dutyCycle from angle
-  Serial.print("dutyCycle: ");
-  Serial.println(dutyCycle);
+  float dutyCycle = (Servo.dc_max - Servo.dc_min)/180 * angle + Servo.dc_min; //calculates dutyCycle from angle
+  //Serial.print("dutyCycle: ");
+  //Serial.println(dutyCycle);
   Servo.PWM_instance->setPWM(Servo.pin, frequency, dutyCycle); //sets dutyCycle in servo
 }
 
@@ -61,10 +45,25 @@ void setup()
 
 void loop()
 {
-  
+  /*
+  float angle = 0;
+  angle = Serial.read();
+  */
+
   setServo(Servo1, 90);
   setServo(Servo2, 90);
   setServo(Servo3, 90);
   setServo(Servo4, 90);
+
+  /*
+  delay(1000);
+
+  setServo(Servo1, 0);
+  setServo(Servo2, 0);
+  setServo(Servo3, 0);
+  setServo(Servo4, 0);
+
+  delay(1000);
+  */
   
 }
